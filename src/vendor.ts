578e76eebab058e0be995f2f690f0b669e867755
markballenger <mark.ballenger@e-hps.com>
2017-01-03 13:53:49 -0500

    otf loader

--- src/vendor.ts
+++ src/vendor.ts
@@ -27,5 +27,5 @@ import 'font-awesome/fonts/fontawesome-webfont.svg';
 import 'font-awesome/fonts/fontawesome-webfont.ttf';
 import 'font-awesome/fonts/fontawesome-webfont.woff';
 import 'font-awesome/fonts/fontawesome-webfont.woff2';
-import 'font-awesome/fonts/fontawesome.otf';
+//import 'font-awesome/fonts/fontawesome.otf';
 
